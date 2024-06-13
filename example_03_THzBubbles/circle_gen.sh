4fe73cf029a3f1e1b474bd73e3d53f3c5a5173b9
Robert Young <robb.young.2011@gmail.com>
2024-06-13 10:47:16 -0400

    updated files to Update 1

---
+++ example_03_THzBubbles/circle_gen.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 69 -s ./sequence_files/seq_P5_N069.txt -t 10.5 -o circ_P5_tw105_n1
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 69 -s ./sequence_files/seq_P5_N069.txt -t 10.0 -o circ_P5_tw100_n1
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 69 -s ./sequence_files/seq_P5_N069.txt -t 11.0 -o circ_P5_tw110_n1
+
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 70 -s ./sequence_files/seq_T7_N070.txt -t 10.5 -o circ_T7_tw105_n1
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 70 -s ./sequence_files/seq_T7_N070.txt -t 10.0 -o circ_T7_tw100_n1
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 70 -s ./sequence_files/seq_T7_N070.txt -t 11.0 -o circ_T7_tw110_n1
+
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 86 -s ./sequence_files/seq_AdMLP_N086.txt -t 10.5 -o circ_AdMLP_tw105_n1
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 86 -s ./sequence_files/seq_AdMLP_N086.txt -t 10.0 -o circ_AdMLP_tw100_n1
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 86 -s ./sequence_files/seq_AdMLP_N086.txt -t 11.0 -o circ_AdMLP_tw110_n1
+
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 138 -s ./sequence_files/seq_P5_N138.txt -t 10.5 -o circ_P5_tw105_n2
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 138 -s ./sequence_files/seq_P5_N138.txt -t 10.0 -o circ_P5_tw100_n2
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 138 -s ./sequence_files/seq_P5_N138.txt -t 11.0 -o circ_P5_tw110_n2
+
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 140 -s ./sequence_files/seq_T7_N140.txt -t 10.5 -o circ_T7_tw105_n2
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 140 -s ./sequence_files/seq_T7_N140.txt -t 10.0 -o circ_T7_tw100_n2
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 140 -s ./sequence_files/seq_T7_N140.txt -t 11.0 -o circ_T7_tw110_n2
+
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 172 -s ./sequence_files/seq_AdMLP_N172.txt -t 10.5 -o circ_AdMLP_tw105_n2
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 172 -s ./sequence_files/seq_AdMLP_N172.txt -t 10.0 -o circ_AdMLP_tw100_n2
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 172 -s ./sequence_files/seq_AdMLP_N172.txt -t 11.0 -o circ_AdMLP_tw110_n2
+
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 207 -s ./sequence_files/seq_P5_N207.txt -t 10.5 -o circ_P5_tw105_n3
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 207 -s ./sequence_files/seq_P5_N207.txt -t 10.0 -o circ_P5_tw100_n3
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 207 -s ./sequence_files/seq_P5_N207.txt -t 11.0 -o circ_P5_tw110_n3
+
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 210 -s ./sequence_files/seq_T7_N210.txt -t 10.5 -o circ_T7_tw105_n3
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 210 -s ./sequence_files/seq_T7_N210.txt -t 10.0 -o circ_T7_tw100_n3
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 210 -s ./sequence_files/seq_T7_N210.txt -t 11.0 -o circ_T7_tw110_n3
+
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 258 -s ./sequence_files/seq_AdMLP_N258.txt -t 10.5 -o circ_AdMLP_tw105_n3
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 258 -s ./sequence_files/seq_AdMLP_N258.txt -t 10.0 -o circ_AdMLP_tw100_n3
+python3 ../circle_generator_script/planar_circle_generator_v02.py -l 258 -s ./sequence_files/seq_AdMLP_N258.txt -t 11.0 -o circ_AdMLP_tw110_n3
